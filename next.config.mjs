aa83db71aa4518661618c062c492e4ffb2dee747
whcender <heilender31@gmail.com>
2024-05-01 00:30:01 +0300

    akjdh

--- next.config.mjs
+++ next.config.mjs
@@ -13,17 +13,7 @@ const nextConfig = {
               value: "*",
               // DOES NOT WORK
               // value: process.env.ALLOWED_ORIGIN,
-            },
-            // Allows for specific methods accepted
-            {
-              key: "Access-Control-Allow-Methods",
-              value: "GET, POST, PUT, DELETE, OPTIONS",
-            },
-            // Allows for specific headers accepted (These are a few standard ones)
-            {
-              key: "Access-Control-Allow-Headers",
-              value: "Content-Type, Authorization",
-            },
+            }
           ],
         },
       ];
